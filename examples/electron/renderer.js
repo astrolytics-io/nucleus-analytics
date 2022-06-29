51759c1c9f61d7d8d67e77fbb6d725e4007d1d0c
Vince Loewe <vincent.loewert@gmail.com>
2022-06-29 14:18:51 +0200

    update user id for electron testing

--- examples/electron/renderer.js
+++ examples/electron/renderer.js
@@ -1,10 +1,9 @@
 const Nucleus = require("nucleus-analytics")
 
-Nucleus.init("5eb6d2cf90556b799d753178", {
-  //"62b04f270d5debec3e5d937b", {
+Nucleus.init("62b04f270d5debec3e5d937b", {
   endpoint: "ws://localhost:5000",
   debug: true,
-  sessionTimeout: 10, // 1 minute
+  //sessionTimeout: 10, // 1 minute
 })
 
 Nucleus.page("home")
